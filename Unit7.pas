6ac77cb5f2b3ef05bcb11e11632bbc66025d0a16
José León <jose.leon@embarcadero.com>
2014-08-19 10:42:41 +0200

    +Added new button

--- Unit7.pas
+++ Unit7.pas
@@ -4,10 +4,12 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls;
 
 type
   TForm7 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +22,10 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm7.Button1Click(Sender: TObject);
+begin
+  showmessage('ok');
+end;
 
 end.
